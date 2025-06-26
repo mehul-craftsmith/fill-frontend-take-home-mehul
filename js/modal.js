da9dbeb5534bc0624e8bda943b57e69977432793
Mehul Craftsmith <mehul.craftsmith@gmail.com>
2025-06-26 20:01:39 +0530

    Add context menu on button click

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 
 const modal = document.getElementById("modal");
 const modalBackdrop = document.getElementById("modal-backdrop");
+const contextMenu = document.getElementById("context-menu");
 
 
 const openModal = () => {
@@ -11,3 +12,18 @@ const closeModal = () => {
     modal.classList.remove("open");
     modalBackdrop.classList.remove("modal-backdrop");
 };
+
+const openContext = () => {
+    contextMenu.classList.add("open");
+};
+
+const closeContext = () => {
+    contextMenu.classList.remove("open");
+};
+
+const btn = document.getElementById("open-context-btn");
+document.addEventListener('click', function (event) {
+    if (!contextMenu.contains(event.target) && !btn.contains(event.target)) {
+        closeContext();
+    }
+});
