68bd1028f9ef0587dd2a28db7309c3f4bab77630
Mehul Craftsmith <mehul.craftsmith@gmail.com>
2025-06-26 13:08:08 +0530

    Adding animations to add menu option

--- add-menu.js
+++ add-menu.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
         gap.addEventListener('mouseenter', () => {
             gaps.forEach(g => g.classList.remove('expand'));
             gap.classList.add('expand');
+
+            // Remove after 1.5 seconds
+            setTimeout(() => {
+                gap.classList.remove('expand');
+            }, 1500);
         });
     });
 });
