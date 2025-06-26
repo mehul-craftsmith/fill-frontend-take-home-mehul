7f6431c8d8b62e8653d7e6ff7fc4901385b962cc
Mehul Craftsmith <mehul.craftsmith@gmail.com>
2025-06-26 15:45:45 +0530

    Make modal more aesthetic

--- modal.js
+++ modal.js
@@ -1,11 +1,13 @@
 
 const modal = document.getElementById("modal");
-
+const modalBackdrop = document.getElementById("modal-backdrop");
 
 
 const openModal = () => {
     modal.classList.add("open");
+    modalBackdrop.classList.add("modal-backdrop");
 };
 const closeModal = () => {
     modal.classList.remove("open");
+    modalBackdrop.classList.remove("modal-backdrop");
 };
